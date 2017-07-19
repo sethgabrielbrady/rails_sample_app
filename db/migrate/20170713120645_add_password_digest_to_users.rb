fd4e73e449e5139a8607a44bb10b1e7a5c31b11a
Administrator <administrator@Administrators-MacBook-Pro.local>
2017-07-19 07:59:33 -0400

    updated active record

--- db/migrate/20170713120645_add_password_digest_to_users.rb
+++ db/migrate/20170713120645_add_password_digest_to_users.rb
@@ -1,5 +1,5 @@
 class AddPasswordDigestToUsers < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :password_digest, :str
+    add_column :users, :password_digest, :string
   end
 end
