ba1b392b69b22bdf3665fc4f6bb0d61b92ab9613
Seth Brady <sethgabrielbrady@gmail.com>
2017-08-08 08:08:49 -0400

    updated the first signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,4 @@
-
+# USER CONTROLLER
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
@@ -11,17 +11,16 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # Handle a successful save.
-    else
-      render 'new'
+      flash[:success] = 'Welcome to the Sample App!'
+      redirect_to @user
+    else render 'new'
     end
   end
 
   private
 
-    def user_params
-      params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
-    end
-
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
+  end
 end
