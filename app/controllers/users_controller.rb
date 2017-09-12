2ae595ab0bf7f640d80506a669fe0f15ce8048fb
Seth Brady <sethgabrielbrady@gmail.com>
2017-09-12 14:13:03 -0400

    updated login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 # USER CONTROLLER
 class UsersController < ApplicationController
+
   def show
     @user = User.find(params[:id])
   end
@@ -11,16 +12,18 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = 'Welcome to the Sample App!'
+      log_in @user
+      flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
-    else render 'new'
+    else
+      render 'new'
     end
   end
 
   private
 
-  def user_params
-    params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
-  end
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
