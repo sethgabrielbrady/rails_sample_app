b893fb9b74d3f0c3efbbbfe7eb23667e808e1167
Seth Brady <sethgabrielbrady@gmail.com>
2017-09-25 08:21:38 -0400

    add password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
   before_create :create_activation_digest
   validates :name,  presence: true, length: { maximum: 50 }
@@ -40,6 +40,8 @@ class User < ApplicationRecord
     SecureRandom.urlsafe_base64
   end
 
+
+
 class << self
   # Returns the hash digest of the given string.
     def digest(string)
@@ -75,6 +77,23 @@ class << self
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
   end
+
+  # Sets the password reset attributes.
+   def create_reset_digest
+     self.reset_token = User.new_token
+     update_attribute(:reset_digest,  User.digest(reset_token))
+     update_attribute(:reset_sent_at, Time.zone.now)
+   end
+
+   # Sends password reset email.
+   def send_password_reset_email
+     UserMailer.password_reset(self).deliver_now
+   end
+
+ # Returns true if a password reset has expired.
+   def password_reset_expired?
+     reset_sent_at < 2.hours.ago
+   end
 
 private
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
 
   get 'password_resets/new'
-
   get 'password_resets/edit'
-
   get 'sessions/new'
   root   'static_pages#home'
-  # get    'users/new'
   get    '/help',    to: 'static_pages#help'
   get    '/about',   to: 'static_pages#about'
   get    '/contact', to: 'static_pages#contact'
