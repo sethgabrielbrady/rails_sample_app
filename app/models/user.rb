60748d038f83b57078d6ee7a6ee5c561a55b52bf
Seth Brady <sethgabrielbrady@gmail.com>
2017-10-04 08:43:59 -0400

    13.1 finished

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+  has_many :microposts
 
   # Returns the hash digest of the given string.
   def User.digest(string)
